ddb96267d2942dd06a9c2fc92fd6ec79c267d548
DhaliaM <keine>
2020-12-23 17:40:31 +0100

    html form muß erweitert werden, braucht mehr infos in der Übergabe an das servlet. Zielausgabe als Bsp
    1:
      Name=HippoCarceres
      Anzahl= n
      Galaxiebonus= Anzahl gesetzter checkboxen
    
    Buttons sind erstellt, chkboxen fehlen noch

--- src/main/java/drunk/homebrew/forge/of/empires/app/BuildingServlet.java
+++ src/main/java/drunk/homebrew/forge/of/empires/app/BuildingServlet.java
@@ -48,21 +48,45 @@ public class BuildingServlet extends HttpServlet {
         sb.append("<body>");
         sb.append("<form action=\"EventBuildings\" name=\"buildings\" id=\"buildings\"");
         sb.append("<ul>");
-        for (String key : eventBuildings.keySet()){
+        for (String key : eventBuildings.keySet()){                                                     //daten aus der map, atm yaml
             sb.append("<li>");
             sb.append("<label>");
-            sb.append(eventBuildings.get(key).getName());
-            sb.append(" <input id=\"");
+            sb.append(eventBuildings.get(key).getName());                                               //Label Name aus der map
+            sb.append(" <input type=\"number\" id=\"");                                                 //input Feld, Id basierned auf der Map
             sb.append(key);
+            sb.append("\" name=\"");
+            sb.append(eventBuildings.get(key).getName());                                               //input Feld Name aus der map, vmtl nutzlos
             sb.append("\" value=\"0\" size=\"1\">");
+            sb.append("<button id=\"");
+            sb.append(key);
+            sb.append(".GalaxieBonus\" ");
+            sb.append("onclick=\"generateGalxieBonusCheckBox(this)\">");
+            sb.append("Galxiebonus");
+            sb.append("</button>");
             sb.append("</label>");
             sb.append("</li>");
         }
         sb.append("<input type=\"Button\" id=\"submitButton\" onclick=\"submitJson()\" value=\"Submit\">");
         sb.append("</form>");
+
         sb.append("<script>");
         sb.append("const form = document.querySelector('form');");
         sb.append("form.addEventListener('submit', event => {event.preventDefault()});");
+        //Funktion um die Id aus dem Button ohne Buchstaben zu erhalten
+        sb.append("function splitButtonString(String string){");
+        sb.append("string = string.split(\".\",1)");
+        sb.append("return string;");
+        sb.append("}");
+        // Funktion um, basierend auf der Anzahl in input.value, checkboxen zu erstellen für den evtl. GalaxieBonus
+        sb.append("function generateGalaxieBonusCheckBox(buttonId){");
+        sb.append("var id = splitButtonString(buttonId.id)");
+        //sb.append("var inputValue = getElementById(id).value;");
+        sb.append("if(getElementById(id).value > 0){");
+        sb.append("for(i = 1; i < getElementById(id).value; i++){");
+
+//        sb.append("console.log()");
+//        function to create checkboxes numbering the input.id.value, named input.id +.+ "gBonus" +.+ for(i=1; 1 to input.id.value  , appenchild Button?
+        sb.append("}");
         sb.append("function submitJson(){");
         sb.append("var dataForm = document.getElementById(\"buildings\");");
         sb.append("var dataInArray = new Map();");
@@ -89,30 +113,17 @@ public class BuildingServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-//        Map<String,Object> result = new ObjectMapper().readValue(request, Map.class);
+//        Map<String,Object> result = new ObjectMapper().readValue(request.getInputStream(), Map.class);
 
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
 
-        PrintWriter pw = response.getWriter();
-        StringBuilder sb = new StringBuilder();
 
-        sb.append("<!DOCTYPE html>");
-        sb.append("<html lang=\"de\">");
-        sb.append("<head>");
-        sb.append("<title>Titel der Seite | Name der Website</title>");
-        sb.append("</head>");
-        sb.append("<body>");
-        sb.append("<p>");
-        sb.append(request);
-        sb.append("</p>");
-        sb.append("</body>");
-        sb.append("<script>");
-        //sb.append("alert ('test')");
-        sb.append("</script>");
-        sb.append("</html>");
 
-        pw.println(sb);
-        pw.close();
+        // to send out the json data
+
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.println(request);
+        out.close();
+
     }
 }
