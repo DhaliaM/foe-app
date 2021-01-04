d9702556da9bd6079a8a33d836afda8963a3fa1e
DhaliaM <keine>
2021-01-04 16:44:36 +0100

    html um css erweitert um Fenster zu halbieren, link input, rechts output response vom doPost() steht zumindest schonmal im console.log, muß noch ins <div>output</div> Umbau der Json in eine nested Variante, atm alles hintereinander (!Größe, !Lesbarkeit, !Umwandlung in Javaobjekt)

--- src/main/java/drunk/homebrew/forge/of/empires/app/BuildingServlet.java
+++ src/main/java/drunk/homebrew/forge/of/empires/app/BuildingServlet.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BuildingServlet extends HttpServlet {
+    private String foeAusgabe = new String("Ausgabe");
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
@@ -34,9 +35,6 @@ public class BuildingServlet extends HttpServlet {
 
         StringBuilder sb = new StringBuilder();
 
-//        TransmitBuildings transmitBuildings = new TransmitBuildings();
-//        String buildings = transmitBuildings.transmitBuildings();
-        //ServletAusgabeTest test = new ServletAusgabeTest();
 
         sb.append("<!DOCTYPE html>");
         sb.append("<html lang=\"de\">");
@@ -44,8 +42,18 @@ public class BuildingServlet extends HttpServlet {
         sb.append("<meta charset=\"utf-8\">");
         sb.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
         sb.append("<title>Titel der Seite | Name der Website</title>");
+        sb.append("<style>");
+        sb.append(".wrapper { ");
+        sb.append("display: grid;");
+        sb.append("grid-template-columns: 40% 60%;");
+        sb.append("gap: 3px;}");
+        sb.append(".input { grid-column-start: 1; grid-column-end: 2; place-self: right; }");
+        sb.append(".output { grid-column-start: 2; grid-column-end: 3; align-content: center; }");
+        sb.append("</style>");
         sb.append("</head>");
         sb.append("<body>");
+        sb.append("<div class=\"wrapper\">");
+        sb.append("<div class=\"input\">");
         sb.append("<form action=\"EventBuildings\" name=\"buildings\" id=\"buildings\"");
         sb.append("<ul>");
         for (String key : eventBuildings.keySet()){                                                     //daten aus der map, atm yaml
@@ -57,51 +65,64 @@ public class BuildingServlet extends HttpServlet {
             sb.append("\" name=\"");
             sb.append(eventBuildings.get(key).getName());                                               //input Feld Name aus der map, vmtl nutzlos
             sb.append("\" value=\"0\" size=\"1\">");
-            sb.append("<button id=\"");
+            sb.append(" Anzahl Galaxiebonus ");
+            sb.append("<input type =\"number\" id=\"");
             sb.append(key);
             sb.append(".GalaxieBonus\" ");
-            sb.append("onclick=\"generateGalxieBonusCheckBox(this)\">");
-            sb.append("Galxiebonus");
-            sb.append("</button>");
+            sb.append("value=\"0\" size=\"1\">");
             sb.append("</label>");
+
             sb.append("</li>");
         }
         sb.append("<input type=\"Button\" id=\"submitButton\" onclick=\"submitJson()\" value=\"Submit\">");
         sb.append("</form>");
+        sb.append("</div>");
+
+        sb.append("<div class=\"output\">");
+
+        sb.append(foeAusgabe);
+
+
+        sb.append("</div>");
+
+
 
         sb.append("<script>");
         sb.append("const form = document.querySelector('form');");
         sb.append("form.addEventListener('submit', event => {event.preventDefault()});");
-        //Funktion um die Id aus dem Button ohne Buchstaben zu erhalten
-        sb.append("function splitButtonString(String string){");
-        sb.append("string = string.split(\".\",1)");
-        sb.append("return string;");
-        sb.append("}");
-        // Funktion um, basierend auf der Anzahl in input.value, checkboxen zu erstellen für den evtl. GalaxieBonus
-        sb.append("function generateGalaxieBonusCheckBox(buttonId){");
-        sb.append("var id = splitButtonString(buttonId.id)");
-        //sb.append("var inputValue = getElementById(id).value;");
-        sb.append("if(getElementById(id).value > 0){");
-        sb.append("for(i = 1; i < getElementById(id).value; i++){");
 
-//        sb.append("console.log()");
-//        function to create checkboxes numbering the input.id.value, named input.id +.+ "gBonus" +.+ for(i=1; 1 to input.id.value  , appenchild Button?
-        sb.append("}");
+        //Funktion um die Id aus dem Button ohne Buchstaben zu erhalten, war ursprünglich für etwas anderes
+//        sb.append("function splitButtonString(String string){");
+//        sb.append("string = string.split(\".\",1)");
+//        sb.append("return string;");
+//        sb.append("}");
+
+        //Funktion um die Json zu füllen
         sb.append("function submitJson(){");
         sb.append("var dataForm = document.getElementById(\"buildings\");");
         sb.append("var dataInArray = new Map();");
-        sb.append("for(let element of dataForm.elements){");
-        sb.append("dataInArray[element.id] = element.value;");
-        sb.append("}");
+        sb.append("for(let element of dataForm.elements){");                    //ist 1=0  und nachdem alle Gebäude durch sind 1.GalaxieBonus=1
+        sb.append("dataInArray[element.id] = element.value;");                  //soll 1:
+        sb.append("}");                                                         //      Anzahl=2
+                                                                                //      GBonus=1
         sb.append("delete dataInArray[\"submitButton\"];"); //entfernt den Submit Button aus der Map dataInArray
-        //sb.append("console.log(Object.values(dataInArray));");
+        sb.append("console.log(Object.values(dataInArray));");
         sb.append("var dataJson = JSON.stringify(dataInArray);");
         sb.append("var xhr = new XMLHttpRequest();");
+        sb.append("xhr.onreadystatechange = function() {");
+        sb.append("if (this.readyState == 4 && this.status == 200) {");
+        sb.append("console.log(this.responseText);"); //enthält response von doPost, <div>output mit response füllen somehow
+        sb.append("}");
+        sb.append("};");
         sb.append("xhr.open(\"POST\", \"/FoE/EventBuildings\", true);");
         sb.append("xhr.setRequestHeader('Content-Type', 'application/json');");
         sb.append("xhr.send(dataJson);");
-        //sb.append("alert (dataJson);"); //nur zum testen was in dataJson drin steht
         sb.append("}");
+
+        sb.append("document.getElementById(\"buildings\").onsubmit = function(){");
+        sb.append("location.reload(true);");
+        sb.append("}");
+
         sb.append("</script>");
         sb.append("</body>");
         sb.append("</html>");
@@ -113,17 +134,20 @@ public class BuildingServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-//        Map<String,Object> result = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+        Map<String,Object> result = new ObjectMapper().readValue(request.getInputStream(), Map.class);
 
 
-
+        foeAusgabe = "test";
 
         // to send out the json data
+//
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter pw = response.getWriter();
+        pw.println(result);
+        pw.close();
 
-        response.setContentType("application/json");
-        PrintWriter out = response.getWriter();
-        out.println(request);
-        out.close();
-
+//          doGet(request,response);
+        response.sendRedirect("/FoE/EventBuildings");
     }
 }
